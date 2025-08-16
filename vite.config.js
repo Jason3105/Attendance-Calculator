67c80ffcb1890fdbedd9c76370e949b6c3cefef8
Jason3105 <jasongons312005@gmail.com>
2025-08-16 18:03:55 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {},
+  server: {},
+  // Add this for SPA fallback
+  preview: {
+    // For Render, fallback to index.html
+    fallback: 'index.html'
+  }
 })
